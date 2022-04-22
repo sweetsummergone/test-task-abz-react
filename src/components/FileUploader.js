19f06fe796abb59ed168fab095232d05c2213f25
sweetsummergone <limfix.13@gmail.com>
2022-04-22 18:34:08 +0300

    fix: set FileUploader component to display styles if file passed/not validation

--- src/components/FileUploader.js
+++ src/components/FileUploader.js
@@ -1,6 +1,8 @@
 import React from "react"
 
 export default function FileUploader() {
+    const [isValid, setIsValid] = React.useState(true);
+
     function handleUploadFile(file) {
         const fileTypes = [
             "image/apng",
@@ -14,18 +16,18 @@ export default function FileUploader() {
             "image/webp",
             "image/x-icon"
         ];
-        const input = document.querySelector('.form__file-uploader');
         const para = document.querySelector('.form__preview');
-
-        input.style.opacity = 0;
 
         if (file === undefined) {
             para.placeholder = 'Upload your photo';
         } else {
             if (validFileType(file)) {
+                setIsValid(true);
                 para.value = `${file.name}`;
             } else {
-                para.value = `Not a valid file type.`;
+                setIsValid(false);
+                para.value = "";
+                para.placeholder = `Not a valid file type.`;
             }
         }
 
@@ -37,26 +39,22 @@ export default function FileUploader() {
     React.useEffect(() => {
         const input = document.querySelector('.form__file-uploader');
 
-        function setupEventListener(e) {
-            handleUploadFile(e.target.files[0]) 
-        }
-
         input.addEventListener("change", e => {
-            setupEventListener(e)
+            handleUploadFile(e.target.files[0])
         });
-        
+
         return () => {
             input.removeEventListener("change", e => {
-                setupEventListener(e);
-            })
+                handleUploadFile(e.target.files[0])
+            });
         }
-    }, [])
+    }, []);
 
     return (
         <div className="form__file-element">
-            <label className="form__file-label" htmlFor="image_uploads">Upload</label>
+            <label className={isValid ? "form__file-label form__file-label_regular" : "form__file-label form__file-label_invalid"} htmlFor="image_uploads">Upload</label>
             <input type="file" className="form__file-uploader" id="image_uploads" name="image_uploads" accept=".jpg, .jpeg, .png" />
-            <input type="text" name="file-text" className="input form__preview" placeholder="Upload your photo" readOnly />
+            <input type="text" name="file-text" className={isValid ? "input form__preview" : "input form__preview input_invalid"} placeholder="Upload your photo" readOnly />
         </div>
     )
 }
--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,7 @@
 export default function Input(props) {
     return (
         <label htmlFor={props.name}>
-            <input type="text" name={props.name} id={props.name} className="input" placeholder={props.text} required />
+            <input type={props.type} name={props.name} id={props.name} className="input" placeholder={props.text} required />
             <span>{props.text}</span>
         </label>
     )
